6bbd2f3f61f76a988f8d818bfcf4629243c64383
Kyran Rana <kyran.rana@hotmail.com>
2025-08-15 00:34:12 +0100

    github workflow changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  timeout: 60000, // 60 seconds per test
   webServer: {
     command: 'npm run build && npm run preview',
     port: 4173
